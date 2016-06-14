289428cb7b5a2ba900d8b4a53158c7c0fb5105bd
Martin B. <chuma2150@gmail.com>
2016-06-14 17:25:55 +0200

    custom directive added

--- AngularJSWorkshop/src/components/contact-list/index.js
+++ AngularJSWorkshop/src/components/contact-list/index.js
@@ -3,6 +3,7 @@ import uiRouter from 'angular-ui-router';
 import contactListState from './contact-list-state';
 import ContactListController from './contact-list-controller';
 import ContactListFilter from './contact-list-filter';
+import ContactTemplate from './contact-form.html';
 
 
 const dependencies = [
@@ -13,4 +14,10 @@ export default angular
     .module('ContactList', dependencies)
     .config(contactListState)
     .controller('ContactListController', ContactListController)
-    .filter('capatalize', ContactListFilter);
+    .filter('capatalize', ContactListFilter)
+    .directive("contactForm", function () {
+        return {
+            restrict: 'E',
+            template: ContactTemplate
+        };
+    });
