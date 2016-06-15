51a9872c0ac3ae481abe083b56812614788a4953
Martin B <chuma2150@gmail.com>
2016-06-15 16:45:39 +0200

    Linking from Contact to ContactDetail changed to select object due to given parameter

--- AngularJSWorkshop/src/components/contact-detail/contact-detail-controller.js
+++ AngularJSWorkshop/src/components/contact-detail/contact-detail-controller.js
@@ -1,10 +1,19 @@
-﻿class ContactDetailController {
-    constructor($stateParams) {
+﻿var contactJson = '/contact-list.json';
 
-        this.contactDetail = { name: $stateParams.name, email: $stateParams.email, phone: $stateParams.phone};
+class ContactDetailController {
+
+    constructor($stateParams, $http) {
+
+        this.contactList = [];
+
+        var contactListController = this;
+        $http.get(contactJson).success(function (data) {
+            contactListController.contactList = data.contactList
+                .filter(function (contact) { return contact.name.match(new RegExp($stateParams.name, "i")); });
+        });
     }
 }
 
 // wenn depend. dann davor (das ist ein Array ;))
 // Da hier der state schon angegeben wurde, muss er bei index nicht mehr mit angegeben werden
-export default ['$stateParams', ContactDetailController];
+export default ['$stateParams', '$http', ContactDetailController];
--- AngularJSWorkshop/src/components/contact-list/contact-list-controller.js
+++ AngularJSWorkshop/src/components/contact-list/contact-list-controller.js
@@ -1,21 +1,30 @@
-﻿//import ContactList from './contact-list.json';
+﻿var contactJson = '/contact-list.json';
 
 class ContactListController {
 
     constructor($http) {
 
+        this.http = $http;
         this.contact = {};
         this.contactList = [];
 
         var contactListController = this;
-        $http.get('/contact-list.json').success(function (data) {
-            contactListController.contactList = data.contactList;
+        $http.get(contactJson)
+            .success(function (data) {
+                contactListController.contactList = data.contactList;
         });
     }
 
     add() {
+        this.errors = null;
+        var contactListcontroller = this;
+        this.http.post(contactJson, this.contact)
+            .catch(function (contact) {
+                contactListcontroller.errors = contact.data.error; // funktioniert leider nicht?
+            });
+
         this.contactList.push(this.contact);
-        this.contact = { };
+        this.contact = {};
     }
 }
 
